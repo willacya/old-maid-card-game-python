88aa46c8432dc26ce80386a5397ce59eeed9f11f
willacya <andrewwillacy@gmail.com>
2021-03-24 12:15:52 +0000

    Old Maid game hand class added

--- package/package_name/python_code.py
+++ package/package_name/python_code.py
@@ -133,3 +133,18 @@ class CardGame:
     def __init__(self):
         self.deck = Deck()
         self.deck.shuffle()
+        
+# inherits from Hand class        
+class OldMaidHand(Hand):
+    def remove_matches(self):
+        count = 0
+        original_cards = self.cards[:]
+        for card in original_cards:
+            match = Card(3 - card.suit, card.rank)
+            if match in self.cards:
+                self.cards.remove(card)
+                self.cards.remove(match)
+                print("Hand {0}: {1} matches {2}"
+                        .format(self.name, card, match))
+                count += 1
+        return count
