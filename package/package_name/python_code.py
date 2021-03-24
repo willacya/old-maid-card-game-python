781b33a462e859b80f5cd4efb2941c474b879019
willacya <andrewwillacy@gmail.com>
2021-03-24 10:55:38 +0000

    Created class that prints card hand

---
+++ package/package_name/python_code.py
@@ -0,0 +1,11 @@
+class Card:
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    ranks = ["narf", "Ace", "2", "3", "4", "5", "6", "7",
+             "8", "9", "10", "Jack", "Queen", "King"]
+
+    def __init__(self, suit=0, rank=0):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return (self.ranks[self.rank] + " of " + self.suits[self.suit])
